6c19fbcb84ea67671ddd2c2421c9cb86f1fb9517
Sajan <sajanghuman18@gmail.com>
2023-11-10 09:05:39 -0600

    Modified the header to allow Cross platform data transfer from react to php and vice versa

--- back-end/addFl.php
+++ back-end/addFl.php
@@ -1,9 +1,9 @@
 <?php
 require('connect.php');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Headers: Content-Type');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type');
-header('Content-Type: application/json');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $json = file_get_contents('php://input');
